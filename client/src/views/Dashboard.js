81796b58004fa56e589fc82182a1fa8f78c53ab9
Valentin Molina <valentinmolina83@gmail.com>
2021-11-26 17:36:14 -0800

    Implement part of the add session workflow

--- client/src/views/Dashboard.js
+++ client/src/views/Dashboard.js
@@ -1,12 +1,48 @@
-import { Grid, Box, Flex, Text, Stack, Heading, Divider } from "@chakra-ui/layout";
-import { GridItem, Icon, Image, ScaleFade, Button } from "@chakra-ui/react";
+import {
+  Grid,
+  Box,
+  Flex,
+  Text,
+  Stack,
+  Heading,
+  Divider,
+} from "@chakra-ui/layout";
+import {
+  GridItem,
+  Icon,
+  Image,
+  ScaleFade,
+  Button,
+  Input,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+  Select,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  NumberIncrementStepper,
+  NumberDecrementStepper,
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+} from "@chakra-ui/react";
 
+import { useState } from "react";
 
 import HomeIcon from "@material-ui/icons/HomeOutlined";
 import PeopleIcon from "@material-ui/icons/PeopleOutlined";
 import DateRangeIcon from "@material-ui/icons/DateRangeOutlined";
+import ArrowRight from "@material-ui/icons/ArrowForward";
+import ArrowLeft from "@material-ui/icons/ArrowBack"
 import AnnouncementOutlinedIcon from "@material-ui/icons/AnnouncementOutlined";
-import PlusIcon from "@material-ui/icons/Add"
+import PlusIcon from "@material-ui/icons/Add";
 import ProfileImage from "../images/bob.jpeg";
 const SIDEBAR_ITEMS = [
   {
@@ -15,7 +51,7 @@ const SIDEBAR_ITEMS = [
     selected: true,
   },
   {
-    title: "Class Schedules",
+    title: "Sessions",
     icon: DateRangeIcon,
   },
   {
@@ -28,12 +64,23 @@ const SIDEBAR_ITEMS = [
   },
 ];
 
+const DEMO_STATES = {
+  INIT: "INIT",
+  ADD_SESSION: "ADD_SESSION",
+  ADD_INSTRUCTORS: "ADD_INSTRUCTORS",
+  ADD_COURSES: "ADD_COURSES",
+};
+
 function Dashboard() {
   const renderSidebarItems = () => {
     return SIDEBAR_ITEMS.map((item) => {
       const Icon = item.icon;
       const selectedStyles = item.selected
-        ? { color: "purple.500", borderLeftColor: "purple.500", borderLeftWidth: "3px" }
+        ? {
+            color: "purple.500",
+            borderLeftColor: "purple.500",
+            borderLeftWidth: "3px",
+          }
         : {};
       return (
         <Flex
@@ -42,29 +89,164 @@ function Dashboard() {
           alignItems="center"
           color="#868e96"
           p="0.5rem 0.75rem"
-      
           minW="12rem"
           fontSize="1.25rem"
-          
           borderRadius={"0 0.5rem 0.5rem 0"}
           _hover={{ background: "gray.100" }}
           {...selectedStyles}
         >
           <Icon fontSize="inherit" />
 
-          <Text ml="0.5rem" fontWeight={item.selected ? "700" : "600"} fontSize="1rem">
+          <Text
+            ml="0.5rem"
+            fontWeight={item.selected ? "700" : "600"}
+            fontSize="1rem"
+          >
             {item.title}
           </Text>
         </Flex>
       );
     });
   };
+
+  const [currState, setCurrState] = useState(DEMO_STATES.INIT);
+  const [instructors, setInstructors] = useState([
+    {
+      name: "Luis De La Torre",
+      email: "luis.delatorre@wsu.edu",
+      isAdjunct: false
+    },
+    {
+      name: "John Miller",
+      email: "jmiller16@wsu.edu",
+      isAdjunct: false
+    },
+  ])
+  const addSession = () => {
+    /* Fake form for now but you get the idea sir */
+    return (
+      <Box h="100%" w="100%" p="4rem 2rem" maxW="40rem">
+        <Heading as="h2" fontSize="2rem" fontFamily="Merriweather">
+          Specify Time
+        </Heading>
+        <Text color="gray.600">Use season and year.</Text>
+        <Grid mt="2rem" gridTemplateColumns={"1fr 1fr"} columnGap={"1rem"}>
+          <Text fontWeight={"semibold"} gridColumn={"1/3"}>
+            Semester
+          </Text>
+
+          <FormControl id="season">
+            <FormLabel color="gray.600" fontSize={"0.825rem"}>
+              Season
+            </FormLabel>
+            <Select placeholder="Select option">
+              <option value="option1">Spring</option>
+              <option value="option2">Summer</option>
+              <option value="option3">Fall</option>
+              <option value="winter">Winter</option>
+            </Select>
+          </FormControl>
+
+          <FormControl id="email">
+            <FormLabel color="gray.600" fontSize={"0.825rem"}>
+              Year
+            </FormLabel>
+            <NumberInput
+              defaultValue={parseInt(new Date().getFullYear())}
+              min={parseInt(new Date().getFullYear())}
+            >
+              <NumberInputField />
+              <NumberInputStepper>
+                <NumberIncrementStepper />
+                <NumberDecrementStepper />
+              </NumberInputStepper>
+            </NumberInput>
+          </FormControl>
+        </Grid>
+
+        <Button
+          onClick={() => setCurrState(DEMO_STATES.ADD_INSTRUCTORS)}
+          mt="2rem"
+          w="100%"
+          colorScheme={"purple"}
+          rightIcon={<ArrowRight />}
+        >
+          Continue
+        </Button>
+      </Box>
+    );
+  };
+
+  const addInstructors = () => (
+    <Box h="100%" w="100%" p="4rem 2rem">
+      <Heading as="h2" fontSize="2rem" fontFamily="Merriweather">
+        Specify Instructors
+      </Heading>
+      <Text color="gray.600">Add or remove instructors.</Text>
+      <Button m="2rem 0 1rem 0" leftIcon={<PlusIcon />}>Add Instructor</Button>
+      <Table variant="simple">
+        <Thead>
+          <Tr>
+            <Th>Name</Th>
+            <Th>Email</Th>
+            <Th>Is Adjunct?</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {instructors.map((item) => (
+            <Tr>
+              <Td>{item.name}</Td>
+              <Td>{item.email}</Td>
+              <Td>{item.isAdjunct ? "Yes" : "No"}</Td>
+            </Tr>
+          ))}
+        </Tbody>
+        
+      </Table>
+      
+      <Flex justifyContent={"space-between"}>
+      <Button
+        mt="2rem"
+        colorScheme={"purple"}
+        leftIcon={<ArrowLeft />}
+        onClick={() => setCurrState(DEMO_STATES.ADD_SESSION)}
+      >
+        Back
+      </Button>
+      <Button
+        mt="2rem"
+        colorScheme={"purple"}
+        rightIcon={<ArrowRight />}
+      >
+        Continue
+      </Button>
+      </Flex>
+      
+    </Box>
+  );
+
+  const renderContent = () => {
+    switch (currState) {
+      case DEMO_STATES.INIT:
+        return <Text>Nothing to see here...</Text>;
+
+      case DEMO_STATES.ADD_SESSION:
+        return addSession();
+
+      case DEMO_STATES.ADD_INSTRUCTORS:
+        return addInstructors();
+    }
+  };
+
   return (
     <ScaleFade initialScale={0.9} in={true} w="100%" h="100%">
-      <Grid w="100%" h="100%"  bg="#f9f9fd" templateColumns="18rem 1fr">
-
+      <Grid w="100%" h="100%" bg="#f9f9fd" templateColumns="18rem 1fr">
         {/* Sidebar */}
-        <GridItem as="aside" bg="white" boxShadow="rgba(149, 157, 165, 0.2) 0px 8px 24px">
+        <GridItem
+          as="aside"
+          bg="white"
+          boxShadow="rgba(149, 157, 165, 0.2) 0px 8px 24px"
+        >
           <Box p="2rem 1.5rem">
             <Heading
               fontSize="1.5rem"
@@ -77,7 +259,14 @@ function Dashboard() {
             </Heading>
             <Stack spacing="0.75rem">{renderSidebarItems()}</Stack>
             <Divider my="1rem" />
-            <Button colorScheme="purple" w="100%" leftIcon={<PlusIcon />}>Add Schedule</Button>
+            <Button
+              onClick={() => setCurrState(DEMO_STATES.ADD_SESSION)}
+              colorScheme="purple"
+              w="100%"
+              leftIcon={<PlusIcon />}
+            >
+              Add Session
+            </Button>
           </Box>
         </GridItem>
 
@@ -91,28 +280,18 @@ function Dashboard() {
             w="100%"
           >
             <Flex alignItems="center">
-              {/* <Heading
-                as="h1"
-                fontWeight="700"
-                fontSize="1.75rem"
-                alignSelf="end  "
-                fontFamily="Merriweather"
-              >
-                Welcome back Bob Lewis!
-              </Heading> */}
-
               {/* User profile items */}
               <Grid
                 ml="auto"
                 gridTemplateRows="auto auto"
                 columnGap="1rem"
-                bg="white" boxShadow="rgba(149, 157, 165, 0.2) 0px 8px 24px"
+                bg="white"
+                boxShadow="rgba(149, 157, 165, 0.2) 0px 8px 24px"
                 padding="0.5rem 1rem"
                 borderRadius="0.5rem"
                 gridTemplateColumns="auto auto"
               >
                 <GridItem
-              
                   alignSelf="end"
                   gridRow="1"
                   gridColumn="1"
@@ -151,9 +330,9 @@ function Dashboard() {
               borderRadius="0.5rem"
               d="flex"
               justifyContent="center"
-              alignItems="center"              
+              alignItems="center"
             >
-              <Text>Nothing to see here...</Text>
+              {renderContent()}
             </GridItem>
           </Grid>
         </GridItem>
